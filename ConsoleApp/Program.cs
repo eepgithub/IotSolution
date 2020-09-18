8f27d0339518bac0a60577c771a5dd47f63ee221
Emil <emilpppe@gmail.com>
2020-09-18 12:05:45 +0200

    Uppgift 3, G del

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using Microsoft.Azure.Devices.Client;
+using SharedLibraries.Models;
+using SharedLibraries.Services;
+using System;
+
+
 
 namespace ConsoleApp
 {
     class Program
     {
+        private static readonly string _conn = "HostName=EC-iothub.azure-devices.net;DeviceId=consoleapp;SharedAccessKey=Wpp+e1YG/15ffM0XYiK61eeSSVT6ltskDc97X7EktEk=";
+
+        private static readonly DeviceClient deviceClient = DeviceClient.CreateFromConnectionString(_conn, TransportType.Mqtt);
+
+
         static void Main(string[] args)
         {
-            
+
+            DeviceService.SendMessageAsync(deviceClient).GetAwaiter();
+            DeviceService.ReceiveMessageAsync(deviceClient).GetAwaiter();
+
+
+
+            Console.ReadKey();
         }
     }
 }
---
+++ SharedLibraries/Services/DeviceService.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MAD = Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using SharedLibraries.Models;
+
+namespace SharedLibraries.Services
+{
+    public static class DeviceService
+    {
+
+        private static readonly Random rnd = new Random();
+
+
+        // Device Client = IoT Device
+        public static async Task ReceiveMessageAsync(DeviceClient deviceClient)
+        {
+            while(true)
+            {
+                var payload = await deviceClient.ReceiveAsync();
+
+                if (payload == null)
+                    continue;
+
+                Console.WriteLine($"Message Received: {Encoding.UTF8.GetString(payload.GetBytes())}");
+
+                await deviceClient.CompleteAsync(payload);
+            }
+        }
+        
+        // Device Client = IoT Service
+        public static async Task SendMessageAsync(DeviceClient deviceClient)
+        {
+            while(true)
+            {
+                var data = new TemperatureModel
+                {
+                    Temperature = rnd.Next(20, 30),
+                    Humidity = rnd.Next(40, 50)
+                };
+
+
+
+                //JSON = {"temperature": 20, "humidity": 44}
+                var json = JsonConvert.SerializeObject(data);
+
+                var payload = new Microsoft.Azure.Devices.Client.Message(Encoding.UTF8.GetBytes(json));
+                await deviceClient.SendEventAsync(payload);
+
+                Console.WriteLine($"Message sent: {json}");
+
+
+
+                await Task.Delay(10 * 1000);
+            }
+        }
+
+        // Service Client = IoT Hub
+        public static async Task SendMessageToDeviceAsync(MAD.ServiceClient serviceClient, string targetDeviceID, string message)
+        {
+            var payload = new MAD.Message(Encoding.UTF8.GetBytes(message));
+            await serviceClient.SendAsync(targetDeviceID, payload);
+        }
+
+
+    }
+}
